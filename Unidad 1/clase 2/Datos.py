d5ca741b082a3f4093d9cf8538f428f149ef26d5
Karatsuyu <tabjulian07@gmail.com>
2024-02-22 20:37:28 -0500

    Subiendo cambios

---
+++ Unidad 1/clase 2/Datos.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
+
+principal = Tk()
+principal.title('Datos')
+
+
+frame = Frame(principal, width=500, height=500)
+lblTitulo = tk.Label(frame,text='Crear Usuario')
+lblTitulo.grid(row=0,column =1)
+lblCedula = tk.Label(frame, text='Cedula*:')
+lblCedula.grid(row=2, column=0)
+lblNombre = tk.Label(frame, text='Nombre*:')
+lblNombre.grid(row=3, column=0)
+lblApellido = tk.Label(frame, text='Apellido*:')
+lblApellido.grid(row=4, column=0)
+lblTelefono = tk.Label(frame, text='Telefono*:')
+lblTelefono.grid(row=5,column=0)
+lblCorreoElectronico = tk.Label(frame, text='Correo Electrónico')
+lblCorreoElectronico.grid(row=6, column=0)
+txtCedula = tk.Entry(frame, width=20)
+txtCedula.grid(row=2,column=1)
+txtNombre = tk.Entry(frame, width=20)
+txtNombre.grid(row=3,column=1)
+txtApellido = tk.Entry(frame, width=20)
+txtApellido.grid(row=4,column=1)
+txtTelefono = tk.Entry(frame, width=20)
+txtTelefono.grid(row=5,column=1)
+txtCorreoElectronico = tk.Entry(frame, width=20)
+txtCorreoElectronico.grid(row=6,column=1)
+
+frame.pack()
+
+principal.mainloop()
+
+
+
+
+
--- Unidad 1/clase 2/miau.py
+++ Unidad 1/clase 2/miau.py
@@ -1,11 +1,17 @@
 from tkinter import Frame, Tk
+from tkinter.messagebox import askyesno
+
 
 def eventoDarClick(evento):
     frame.focus_set()
     print('Di click en las coodenadas: ', evento.x, evento.y)
 
 def eventoPresionarTeclado(evento):
-    print("Presione la tecla: ", repr(evento.char) )
+    print("Presione la tecla: ", repr(evento.char))
+
+def usuarioQuiereSalir():
+    if askyesno('Salir de la aplicacion', '¿Esta seguro de uqe quiere cerrar la aplicacion'):
+        principal.destroy()
 
 
 principal = Tk()
@@ -14,6 +20,8 @@ principal.title('Prueba  de eventos')
 frame = Frame(principal, width=500, height=500)
 frame.bind("<Button-1>", eventoDarClick)
 frame.bind("<Key>", eventoPresionarTeclado)
+
 frame.pack()
 
+principal.protocol('WM_DELETE_WINDOW', usuarioQuiereSalir)
 principal.mainloop()
