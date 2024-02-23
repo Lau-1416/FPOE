6be01cd90b962504f0926d16f4969ef445707e04
Karatsuyu <tabjulian07@gmail.com>
2024-02-22 20:05:20 -0500

    Subiendo mis cambios

--- Unidad 1/clase 2/miau.py
+++ Unidad 1/clase 2/miau.py
@@ -1,7 +1,19 @@
 from tkinter import Frame, Tk
+
+def eventoDarClick(evento):
+    frame.focus_set()
+    print('Di click en las coodenadas: ', evento.x, evento.y)
+
+def eventoPresionarTeclado(evento):
+    print("Presione la tecla: ", repr(evento.char) )
+
+
 principal = Tk()
 principal.title('Prueba  de eventos')
 
-frame = Frame(root, width=500, height=500)
+frame = Frame(principal, width=500, height=500)
+frame.bind("<Button-1>", eventoDarClick)
+frame.bind("<Key>", eventoPresionarTeclado)
 frame.pack
+
 principal.mainloop()
