f06b0e415df8caff2c7f323684301568a74e8b71
Karatsuyu <tabjulian07@gmail.com>
2024-02-22 19:50:33 -0500

    Subiendo cambios

--- Unidad 1/clase 2/miau.py
+++ Unidad 1/clase 2/miau.py
@@ -1,2 +1,7 @@
-#Repositorio
-print('Hola Mundo')
+from tkinter import Frame, Tk
+principal = Tk()
+principal.title('Prueba  de eventos')
+
+frame = Frame(root, width=500, height=500)
+frame.pack
+principal.mainloop()
