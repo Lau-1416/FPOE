6e6e0d08a99b42a2fa288d8f37fc41cc186b963c
Karatsuyu <tabjulian07@gmail.com>
2024-02-29 21:24:02 -0500

    Cambios

--- Unidad 1/Clase 3/Datos.py
+++ Unidad 1/Clase 3/Datos.py
@@ -5,18 +5,23 @@ import re
 
 principal =tk.Tk()
 principal.title('Datos')
-principal.geometry("240x220")
+principal.geometry("350x400")
 
 def validarLetras(entrada):
     #Patrón para aceptar solo letras (mayúsculas y minúsculas) y espacios todo lo demas esta restringido y validar nombre debe de dar True
     return re.match("^[a-zA-Z ]*$", entrada) is not None
 
-def validarNombre(nuevoValor):
-    # Solo permitir la entrada si la función de validación de letras devuelve True
-    if validarLetras(nuevoValor) or nuevoValor == "":
+def validarNombre(event=None):
+    nuevoValor = txtNombre.get() # Obtener el valor de la caja de texto
+    if nuevoValor == "":
+        lblAdvertencia.grid_remove() # Ocultar el Label de advertencia si la entrada es vacía
+        return True
+    elif validarLetras(nuevoValor):
+        lblAdvertencia.grid_remove() # Ocultar el Label de advertencia si la entrada es válida
         return True
     else:
-        print(f'El caracter {nuevoValor} no esta permitido')
+        lblAdvertencia.grid(row=2, column=1) # Mostrar el Label de advertencia
+        print(f'Caracter {nuevoValor} no es valido')
         return False
     
 
@@ -26,11 +31,16 @@ def validarLetrasApellido(entrada):
     return re.match("^[a-zA-Z ]*$", entrada) is not None
 
 def validarApellido(nuevoValor):
-    # Solo permitir la entrada si la función de validación de letras devuelve True
-    if validarLetras(nuevoValor) or nuevoValor == "":
+    nuevoValor = txtApellido.get() # Obtener el valor de la caja de texto
+    if nuevoValor == "":
+        lblAdvertencia1.grid_remove() # Ocultar el Label de advertencia si la entrada es vacía
+        return True
+    elif validarLetras(nuevoValor):
+        lblAdvertencia1.grid_remove() # Ocultar el Label de advertencia si la entrada es válida
         return True
     else:
-        print(f'El caracter {nuevoValor} no esta permitido')
+        lblAdvertencia1.grid(row=4,column=1)# Mostrar el Label de advertencia
+        print(f'Caracter {nuevoValor} no es valido')
         return False
     
 
@@ -88,42 +98,55 @@ def validarTecla(event):
 
 
 frame = tk.Frame(principal, padx=10, pady=10)
-lblTitulo = tk.Label(frame, text='Ingresar Datos')
+lblTitulo = tk.Label(frame, text='Complete los campos')
 lblTitulo.grid(row=0, column=0, columnspan=2) 
 
 
 lblNombre = tk.Label(frame, text='Nombre:')
-lblNombre.grid(row=1, column=0, padx=5, pady=5)  
+lblNombre.grid(row=1, column=0, padx=5, pady=10)
+lblAdvertencia = tk.Label(frame, text='Solo se permiten letras')
+#lblAdvertencia.grid(row=2,column=1)
 lblApellido = tk.Label(frame, text='Apellido:')
-lblApellido.grid(row=2, column=0, padx=5, pady=5)
+lblApellido.grid(row=3, column=0, padx=5, pady=10)
+lblAdvertencia1 = tk.Label(frame, text='Solo se permiten letras')
+#lblAdvertencia1.grid(row=4,column=1)
 lblEdad = tk.Label(frame, text='Edad:')
-lblEdad.grid(row=3,column=0, padx=5, pady=5)
+lblEdad.grid(row=5,column=0, padx=5, pady=10)
+lblAdvertencia2 = tk.Label(frame, text='Solo se permiten numeros')
+lblAdvertencia2.grid(row=6,column=1)
 lblCorreoElectronico = tk.Label(frame, text='Correo Electrónico:')
-lblCorreoElectronico.grid(row=4, column=0, padx=5, pady=5)
+lblCorreoElectronico.grid(row=7, column=0, padx=5, pady=10)
+lblAdvertencia3 = tk.Label(frame, text='Solo se permiten(Aa, ., 0-9 ,@)')
+lblAdvertencia3.grid(row=8,column=1)
 lblFechaNacimiento = tk.Label(frame, text='Fecha Nacimiento:')
-lblFechaNacimiento.grid(row=5, column=0, padx=5, pady=5)
+lblFechaNacimiento.grid(row=9, column=0, padx=5, pady=10)
+lblAdvertencia4 = tk.Label(frame, text='Solo se permiten Numeros y "/"')
+lblAdvertencia4.grid(row=10,column=1)
 
 txtNombre = tk.Entry(frame, width=20)
-txtNombre.grid(row=1,column=1, padx=5, pady=5)  
+txtNombre.grid(row=1,column=1, padx=5, pady=10)  
 txtApellido = tk.Entry(frame, width=20)
-txtApellido.grid(row=2,column=1, padx=5, pady=5)
+txtApellido.grid(row=3,column=1, padx=5, pady=10)
 txtEdad = tk.Entry(frame, width=20)
-txtEdad.grid(row=3,column=1, padx=5, pady=5)  
+txtEdad.grid(row=5,column=1, padx=5, pady=10)  
 txtCorreoElectronico = tk.Entry(frame, width=20)
-txtCorreoElectronico.grid(row=4,column=1, padx=5, pady=5)  
+txtCorreoElectronico.grid(row=7,column=1, padx=5, pady=10)  
 txtFechaNacimiento = tk.Entry(frame, width=20)
-txtFechaNacimiento.grid(row=5,column=1, padx=5, pady=5)  
+txtFechaNacimiento.grid(row=9,column=1, padx=5, pady=10)  
 
 #Unir las validaciones a el campo de texto
 
-validacion = principal.register(validarNombre)
-txtNombre.config(validate='key',validatecommand=(validacion, '%P'))
+'''validacion = principal.register(validarNombre)
+txtNombre.config(validate='key',validatecommand=(validacion, '%P'))'''
+txtNombre.bind('<KeyRelease>', validarNombre)
 
 validacion = principal.register(validarEdad)
 txtEdad.config(validate="key", validatecommand=(validacion, '%P'))
 
-validacion = principal.register(validarApellido)
-txtApellido.config(validate='key',validatecommand=(validacion, '%P'))
+'''validacion = principal.register(validarApellido)
+txtApellido.config(validate='key',validatecommand=(validacion, '%P'))'''
+txtApellido.bind('<KeyRelease>', validarApellido)
+
 
 '''validacion = principal.register(validarFecha)
 txtFechaNacimiento.config(validate='key', validatecommand=(validacion, '%P'))'''
