ac9dc050453df6879053723a4d292ebca38eedf3
Karatsuyu <tabjulian07@gmail.com>
2024-03-06 14:53:27 -0500

    Cambios

--- Unidad 1/Clase 3/Datos.py
+++ Unidad 1/Clase 3/Datos.py
@@ -7,88 +7,111 @@ from tkcalendar import DateEntry
 
 principal =tk.Tk()
 principal.title('Datos')
-principal.geometry("350x400")
+principal.geometry("420x400")
 
 frame = tk.Frame(principal, padx=10, pady=10)
 lblTitulo = tk.Label(frame, text='Complete los campos')
 lblTitulo.grid(row=0, column=0, columnspan=2) 
 
+lblAdvertencia = tk.Label(frame, text='Solo se permiten letras',fg="red")
+lblAdvertencia.grid(row=2,column=1, sticky="w")
+lblAdvertencia.grid_remove()
+
+lblAdvertencia1 = tk.Label(frame, text='Solo se permiten letras',fg="red")
+lblAdvertencia1.grid(row=4,column=1, sticky="w")
+lblAdvertencia1.grid_remove()
+
+lblAdvertencia2 = tk.Label(frame, text='Solo se permiten numeros',fg="red")
+lblAdvertencia2.grid(row=6,column=1, sticky="w")
+lblAdvertencia2.grid_remove()
+
+lblAdvertencia3 = tk.Label(frame, text='Solo se permiten(Aa, ., 0-9 ,@)',fg="red")
+lblAdvertencia3.grid(row=8,column=1, sticky="w")
+lblAdvertencia3.grid_remove()
+
+lblAdvertencia4 = tk.Label(frame, text='Solo se permiten Numeros y "/"', fg="red")
+lblAdvertencia4.grid(row=10,column=1, sticky="w")
+lblAdvertencia4.grid_remove()
+
+def mostrarBoton():
+    btnFormularioMal.grid(row=12, columnspan=2, pady=10)
+    
+def mostrarMensaje():
+    messagebox.showerror("Error", "¡El formulario no es válido!")
+
+def ocultarBoton():
+    btnFormularioMal.grid_remove()
+
 
 def validarLetras(entrada):
-    #Patrón para aceptar solo letras (mayúsculas y minúsculas) y espacios todo lo demas esta restringido y validar nombre debe de dar True
     return re.match("^[a-zA-Z ]*$", entrada) is not None
 
 def validarNombre(event=None):
-    nuevoValor = txtNombre.get() # Obtener el valor de la caja de texto
+    nuevoValor = txtNombre.get()
     if nuevoValor == "":
-        lblAdvertencia.grid_remove() # Ocultar el Label de advertencia si la entrada es vacía
+        lblAdvertencia.grid_remove()
+        ocultarBoton()
         return True
     elif validarLetras(nuevoValor):
-        lblAdvertencia.grid_remove() # Ocultar el Label de advertencia si la entrada es válida
+        lblAdvertencia.grid_remove()
+        ocultarBoton()
         return True
     else:
-        lblAdvertencia.grid(row=2, column=1) # Mostrar el Label de advertencia
-        print(f'Caracter {nuevoValor} no es valido')
-        return False
-
-def validar_numeros(entrada):
-    return re.match("^[0-99]*$", entrada) is not None and len(entrada) <= 2
-
-def validar_numeros(entrada):
-    return re.match("^[0-99]*$", entrada) is not None and len(entrada) <= 2
-
-def validarEdad(nueva_edad):
-    if validar_numeros(nueva_edad) or nueva_edad == "":
-        lblAdvertencia2.grid_remove()
-        return True
-    else:
-        lblAdvertencia2.grid(row=6, column=1)
-        print(f'El caracter {nueva_edad} no es valido en este campo')
+        lblAdvertencia.grid(row=2, column=1)
+        mostrarBoton()
         return False
     
-
-#Validar el campo de los apellidos
 def validarLetrasApellido(entrada):
-    #Patrón para aceptar solo letras (mayúsculas y minúsculas) y espacios todo lo demas esta restringido y validar nombre debe de dar True
     return re.match("^[a-zA-Z ]*$", entrada) is not None
 
-def validarApellido(nuevoValor):
-    nuevoValor = txtApellido.get() # Obtener el valor de la caja de texto
+def validarApellido(event=None):
+    nuevoValor = txtApellido.get()
     if nuevoValor == "":
-        lblAdvertencia1.grid_remove() # Ocultar el Label de advertencia si la entrada es vacía
+        lblAdvertencia1.grid_remove()
+        ocultarBoton()
         return True
-    elif validarLetras(nuevoValor):
-        lblAdvertencia1.grid_remove() # Ocultar el Label de advertencia si la entrada es válida
+    elif validarLetrasApellido(nuevoValor):
+        lblAdvertencia1.grid_remove()
+        ocultarBoton()
         return True
     else:
-        lblAdvertencia1.grid(row=4,column=1)# Mostrar el Label de advertencia
-        print(f'Caracter {nuevoValor} no es valido')
+        lblAdvertencia1.grid(row=4,column=1)
+        mostrarBoton()
         return False
-    
 
 def validar_numeros(entrada):
-    return re.match("^[0-99]*$", entrada) is not None and len(entrada) <= 2
+    return re.match("^[0-9]{1,2}$", entrada) is not None and len(entrada) <= 2
 
-def validarEdad(nueva_edad):
-    if validar_numeros(nueva_edad) or nueva_edad == "":
+def validarEdad(event=None):
+    nueva_edad = txtEdad.get()
+    if nueva_edad == '':
         lblAdvertencia2.grid_remove()
+        ocultarBoton()
+        return True
+    elif validar_numeros(nueva_edad):
+        lblAdvertencia2.grid_remove()
+        ocultarBoton()
         return True
     else:
-        lblAdvertencia2.grid(row=6, column=1)
-        print(f'El caracter {nueva_edad} no es valido en este campo')
+        lblAdvertencia2.grid(row=6,column=1)
+        mostrarBoton()
         return False
     
-'''def validarNumerosFecha(entrada):
-    return re.match("^[0-9/]*$", entrada) is not None'''
-
-'''def validarFecha(nuevaFecha):
-    # Patrón para aceptar fechas en el formato DD/MM/AAAA
-    if re.match("^[\d/]{1,10}$", nuevaFecha):
+def validarCorreoElectronico(event=None):
+    email = txtCorreoElectronico.get()
+    if email == '':
+        lblAdvertencia3.grid_remove()
+        ocultarBoton()
+        return True
+    elif re.match(r'^[a-zA-Z0-9.@]+$', email):
+        lblAdvertencia3.grid_remove()
+        ocultarBoton()
         return True
     else:
-        print(f'Carácter no permitido: {"".join(c for c in nuevaFecha if not c.isdigit() and c != "/")}')
-        return False'''
-
+        lblAdvertencia3.grid(row=8, column=1)
+        mostrarBoton()
+        return False
+    
 def cuandoEscriba(event):
     if event.char and event.char.isdigit():
         texto = txtFechaNacimiento.get()
@@ -97,60 +120,63 @@ def cuandoEscriba(event):
             letras += 1
 
         if letras == 2:
-            if int(txtFechaNacimiento.get()[:2]) > 31:  # Verificar día
+            if int(txtFechaNacimiento.get()[:2]) > 31:
                 print("El día debe estar entre 01 y 31")
                 lblAdvertencia4.grid_remove()
+                mostrarBoton()
                 return "break"
             txtFechaNacimiento.insert(2, "/")
         elif letras == 5:
-            if int(txtFechaNacimiento.get()[3:5]) > 12:  # Verificar mes
+            if int(txtFechaNacimiento.get()[3:5]) > 12:
                 print("El mes debe estar entre 01 y 12")
                 lblAdvertencia4.grid_remove()
+                mostrarBoton()
                 return "break"
             txtFechaNacimiento.insert(5, "/")
+        elif letras == 10:
+            txtFechaNacimiento.delete(10, tk.END)  # Limitar la entrada a 10 caracteres
+            ocultarBoton()
         else:
-            lblAdvertencia4.grid_remove()  # Ocultar la advertencia si se ingresa un carácter válido
+            lblAdvertencia4.grid_remove()
+            ocultarBoton()
     elif event.char == '':
-    # Permitir el evento de borrar
-        if len(txtFechaNacimiento.get()) <= 1:
-            lblAdvertencia4.grid_remove()  # Ocultar la advertencia si el campo de fecha está vacío después de borrar
-        return
+        return 
     else:
         print(f'El caracter {event.char} no es válido en este campo')
         lblAdvertencia4.grid(row=10, column=1)
+        mostrarBoton()
         return "break"
-    
-    
-def validarEntrada(text):
-    return all(c.isalpha() or c.isdigit() or c in ".@" for c in text) or text == ""
 
-def validarTecla(event):
-    if event.keysym != "BackSpace" and not validarEntrada(event.char):
-        print(f"El caracter {event.char} no es valido en este campo")
-        return "break"
-    
+def avisoFormulario():
+    messagebox.showinfo('Verificación', 'Formulario completado correctamente')
 
+def validarFormulario():
+    if (validarNombre() and validarApellido() and validarEdad() and validarCorreoElectronico()):
+        mostrarBotonFormularioBien()
+    else:
+        ocultarBotonFormularioBien()
+
+def mostrarMensajeExito():
+    messagebox.showinfo("Éxito", "¡El formulario está completo y correcto!")
+
+def mostrarBotonFormularioBien():
+    if (txtNombre.get() and txtApellido.get() and txtEdad.get() and txtCorreoElectronico.get() and txtFechaNacimiento.get()):
+        ocultarBoton()
+        btnFormularioBien.grid(row=12, columnspan=2, pady=10)
+
+def ocultarBotonFormularioBien():
+    btnFormularioBien.grid_remove()
 
 lblNombre = tk.Label(frame, text='Nombre:')
 lblNombre.grid(row=1, column=0, padx=5, pady=10)
-lblAdvertencia = tk.Label(frame, text='Solo se permiten letras', fg="red")
-#lblAdvertencia.grid(row=2,column=1)
 lblApellido = tk.Label(frame, text='Apellido:')
 lblApellido.grid(row=3, column=0, padx=5, pady=10)
-lblAdvertencia1 = tk.Label(frame, text='Solo se permiten letras', fg="red")
-#lblAdvertencia1.grid(row=4,column=1)
 lblEdad = tk.Label(frame, text='Edad:')
 lblEdad.grid(row=5,column=0, padx=5, pady=10)
-lblAdvertencia2 = tk.Label(frame, text='Solo se permiten numeros', fg="red")
-lblAdvertencia2.grid_remove()
 lblCorreoElectronico = tk.Label(frame, text='Correo Electrónico:')
 lblCorreoElectronico.grid(row=7, column=0, padx=5, pady=10)
-lblAdvertencia3 = tk.Label(frame, text='Solo se permiten(Aa, ., 0-9 ,@)', fg="red")
-lblAdvertencia3.grid(row=8,column=1)
 lblFechaNacimiento = tk.Label(frame, text='Fecha Nacimiento:')
 lblFechaNacimiento.grid(row=9, column=0, padx=5, pady=10)
-lblAdvertencia4 = tk.Label(frame, text='Solo se permiten Numeros y "/"', fg="red")
-lblAdvertencia4.grid_remove()
 
 txtNombre = tk.Entry(frame, width=20)
 txtNombre.grid(row=1,column=1, padx=5, pady=10)  
@@ -163,32 +189,17 @@ txtCorreoElectronico.grid(row=7,column=1, padx=5, pady=10)
 txtFechaNacimiento = DateEntry(frame, date_pattern='dd/mm/yyyy', show_week_numbers=False)
 txtFechaNacimiento.grid(row=9,column=1,padx=5, pady=10)
 
-#Unir las validaciones a el campo de texto
-
-'''validacion = principal.register(validarNombre)
-txtNombre.config(validate='key',validatecommand=(validacion, '%P'))'''
-txtNombre.bind('<KeyRelease>', validarNombre)
-
-validacion = principal.register(validarEdad)
-txtEdad.config(validate="key", validatecommand=(validacion, '%P'))
-
-'''validacion = principal.register(validarApellido)
-txtApellido.config(validate='key',validatecommand=(validacion, '%P'))'''
-txtApellido.bind('<KeyRelease>', validarApellido)
-
-
-'''validacion = principal.register(validarFecha)
-txtFechaNacimiento.config(validate='key', validatecommand=(validacion, '%P'))'''
-
-txtFechaNacimiento.bind("<KeyRelease>", cuandoEscriba)
-txtFechaNacimiento.bind("<BackSpace>", lambda _:txtFechaNacimiento.delete(tk.END))
-
-txtCorreoElectronico.bind("<Key>", validarTecla)
-
-
-
 frame.pack(padx=10, pady=10)
 
+btnFormularioMal = tk.Button(frame, text="Formulario Mal", command=mostrarMensaje)
+btnFormularioBien = tk.Button(frame, text="Formulario Bien", command= mostrarMensajeExito)
+
+
+txtNombre.bind('<KeyRelease>', lambda event: (validarNombre(), validarFormulario()))
+txtApellido.bind('<KeyRelease>', lambda event: (validarApellido(), validarFormulario()))
+txtEdad.bind('<KeyRelease>', lambda event: (validarEdad(), validarFormulario()))
+txtCorreoElectronico.bind('<KeyRelease>', lambda event: (validarCorreoElectronico(), validarFormulario()))
+txtFechaNacimiento.bind("<KeyRelease>", lambda event: (cuandoEscriba(event), validarFormulario()))
+txtFechaNacimiento.bind("<BackSpace>", lambda event: (txtFechaNacimiento.delete(tk.END), validarFormulario()))
 
 principal.mainloop()
-
