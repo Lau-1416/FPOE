e2cb61f1d617f16a89033befa19b7abad07f627d
Karatsuyu <tabjulian07@gmail.com>
2024-04-25 21:26:04 -0500

    Hilos

---
+++ Unidad 2/Clase 10/testHilos.py
@@ -0,0 +1,67 @@
+import threading
+import datetime
+import time
+import logging
+from hilo4 import Hilo4
+from hilo5 import Hilo5
+
+logging.basicConfig(level=logging.DEBUG)
+
+tiempoInicial = datetime.datetime.now()
+
+
+def consultar(nombre):
+    logging.debug('Consultando: ' + nombre)
+    time.sleep(5)
+    return
+
+
+def numeros():
+    list =[1,2,3,4,5]
+    for element in list:
+        logging.debug(element)
+    time.sleep(1)
+    #logging.debug('Consultando: a Julian')
+    return
+
+def letras():
+    list =['a','b','c','d','e']
+    for element in list:
+        logging.debug(element)
+    time.sleep(2)
+    return
+
+hilo1 = threading.Thread(name='hilo1', target=numeros)
+hilo2 = threading.Thread(name='hilo2', target=letras)
+hilo3 = threading.Thread(name='hilo3', target=consultar, args=('Julian', ))
+hilo4 = Hilo4('hilo4', 'Julian')
+hilo5 = Hilo5('hilo5')
+
+
+#hilo1.start()
+#hilo1.join()
+#hilo2.start()
+#hilo2.join()
+#hilo3.start()
+#hilo3.join()
+#hilo4.start()
+hilo5.start()
+
+
+tiempoFinal = datetime.datetime.now()
+
+logging.debug('Tiempo Transcurrido: '  + str(tiempoFinal.second - tiempoInicial.second) + '\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
