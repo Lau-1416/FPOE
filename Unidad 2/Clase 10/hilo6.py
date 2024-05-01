ff866564d240514966c6fa1fbf1e00f8fb651346
Karatsuyu <tabjulian07@gmail.com>
2024-04-30 20:01:50 -0500

    Cambios Hilo

--- Unidad 2/Clase 10/hilo6.py
+++ Unidad 2/Clase 10/hilo6.py
@@ -6,11 +6,11 @@ class Hilo6(threading.Thread):
     def __init__(self, nombre_hilo):
         threading.Thread.__init__(self, name=nombre_hilo)
         self.nombre_hilo = nombre_hilo
-        self.detenido = threading.Event()  #Evento para detener el hilo
+        #self.detenido = threading.Event()  #Evento para detener el hilo
         self.nombrePersona = None #nombrePersona
 
     def run(self):
-        while not self.detenido.is_set():
+        while True:
             self.pedir_nombre()
             self.imprimir_nombre()
             self.guardar_nombre()
@@ -26,21 +26,3 @@ class Hilo6(threading.Thread):
         with open("nombres.txt", "a") as archivo:
             archivo.write(self.nombre + "\n")
 
-    def detener(self):
-        self.detenido.set()
-
-if __name__ == "__main__":
-    hilo = Hilo6("Hilo6")
-    hilo.start()
-
-    hilo5 = Hilo5("Hilo5")
-    hilo5.start()
-
-    try:
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        hilo.detener()
-        hilo5.detener()
-        hilo.join()
-        hilo5.join()
