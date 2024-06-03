3fe3ee898b9fa7b3c0141f790b35113478e59fc1
Lau-1416 <bueno.laura@correounivalle.edu.co>
2024-06-03 17:11:56 -0500

    cambios proyecto

--- Proyecto Final/Front/vistas/vistaPrincipal.py
+++ Proyecto Final/Front/vistas/vistaPrincipal.py
@@ -8,9 +8,25 @@ class LavelopuesApp:
         self.ventana.title("Lavelopues S.A.S.")
         self.ventana.geometry('800x800')
         
-
-
-
+        tk.Label(self.ventana, text="Nombre:").grid(row=0, column=0)
+        self.txtNombre = tk.Entry(self.ventana)
+        self.txtNombre.grid(row=1, column=0)
+        
+        tk.Label(self.ventana, text="Apellido:").grid(row=2, column=0)
+        self.txtApellido = tk.Entry(self.ventana)
+        self.txtApellido.grid(row=3, column=0)
+        
+        tk.Label(self.ventana, text="Cedula:").grid(row=4, column=0)
+        self.txtCedula = tk.Entry(self.ventana)
+        self.txtCedula.grid(row=5, column=0)
+        
+        tk.Label(self.ventana, text="Telefono:").grid(row=6, column=0)
+        self.txtTelefono = tk.Entry(self.ventana)
+        self.txtTelefono.grid(row=7, column=0)
+        
+        tk.Label(self.ventana, text="Correo:").grid(row=8, column=0)
+        self.txtCedula = tk.Entry(self.ventana)
+        self.txtCedula.grid(row=9, column=0)
 
  
 if __name__ == "__main__":
